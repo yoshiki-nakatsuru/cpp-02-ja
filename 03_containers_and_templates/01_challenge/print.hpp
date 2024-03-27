b57bd1844fa804777a190aec5ac1b89fcf4fc7c7
yoshiki-nakatsuru <yoshiki.nakatsuru@woven.toyota>
2024-03-27 21:06:09 +0900

    Submit 01_challenge for grating

--- 03_containers_and_templates/01_challenge/print.hpp
+++ 03_containers_and_templates/01_challenge/print.hpp
@@ -1,1 +1,28 @@
 // コードを入力してください
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+
+template <typename T>
+void print_elements(std::vector<T> vector){
+  for(T t : vector ){
+        std::cout << t ;
+    }
+} 
+
+template <typename T>
+void print_elements(std::list<T> list){
+  for(T t : list ){
+        std::cout << t ;
+    }
+} 
+
+template <typename T>
+void print_elements(std::deque<T> deque){
+  for(T t : deque){
+        std::cout << t ;
+    }
+} 
