02d8d3bca9081ea669534daa34ba2ba369f76ee9
yoshiki-nakatsuru <yoshiki.nakatsuru@woven.toyota>
2024-03-27 21:49:28 +0900

    Submit 02_challenge for grating

--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,26 @@
 // コードを入力してください
+#pragma once
+
+#include <vector>
+#include <list>
+template <typename T>
+class Stack {
+public:
+    void push(const T& value){
+        stack.push_back(value);
+    }
+    void pop(){
+        if(!stack.empty()){
+            stack.pop_back();
+        }
+    }
+    T top() const{
+        return stack.back();
+    }
+    bool is_empty() const{
+        return stack.empty();
+    }
+private:
+    std::vector<T> stack;
+};
+
